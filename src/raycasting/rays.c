6a063a6a048471ab1258785c619922ce469672e0
Achraf Ahrach <aahrach@e1r13p12.1337.ma>
2023-06-16 18:13:47 +0100

    achraf

--- src/raycasting/rays.c
+++ src/raycasting/rays.c
@@ -6,7 +6,7 @@
 /*   By: aahrach <aahrach@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 10:36:20 by ajari             #+#    #+#             */
-/*   Updated: 2023/06/16 12:11:40 by aahrach          ###   ########.fr       */
+/*   Updated: 2023/06/16 17:20:26 by aahrach          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,11 +110,11 @@ void	texters(t_lst m, double dh, double v_x, double v_y)
 	t_textures		*direction = NULL;
 
 	y = 0;
-	if (ft_has_wall(m, v_x, v_y + 1) && !ft_has_wall(m, v_x, v_y - 1))
+	if (ft_has_wall(m, v_x, v_y + 0.005) && !ft_has_wall(m, v_x, v_y - 0.005))
 		direction = m.west;
-	else if (ft_has_wall(m, v_x + 1, v_y) && !ft_has_wall(m, v_x - 1, v_y))
+	else if (ft_has_wall(m, v_x + 0.005, v_y) && !ft_has_wall(m, v_x - 0.005, v_y))
 		direction = m.east;
-	else if (ft_has_wall(m, v_x - 1, v_y) && !ft_has_wall(m, v_x + 1, v_y))
+	else if (ft_has_wall(m, v_x - 0.005, v_y) && !ft_has_wall(m, v_x + 0.005, v_y))
 		direction = m.south;
 	else
 		direction = m.north;
@@ -122,6 +122,8 @@ void	texters(t_lst m, double dh, double v_x, double v_y)
 	start = ceil(HIE / 2 - dh / 2);
 	end = start + dh;
 	x = (int)(direction->w * ((v_x + v_y) / SQ)) % direction->w;
+	//printf("x => %f y = %f (%f)\n", v_x / 20, v_y / 20, (v_x + v_y) / SQ);
+	//exit (0);
 	while (start < end)
 	{
 		color = get_color(direction, x, y);
